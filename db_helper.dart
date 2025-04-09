54330f5a17c115b3c3cd4d9d8afd00ff6f3a9c48
ashh735 <riasatayesha4@gmail.com>
2025-04-08 18:10:48 -0700

    Add files via upload

--- db_helper.dart
+++ db_helper.dart
@@ -1,160 +1,38 @@
-import 'package:flutter/material.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(MyApp());
-}
+class DBHelper {
+  static Database? _db;
 
-// Database Helper Class
-class DatabaseHelper {
-  static final DatabaseHelper instance = DatabaseHelper._init();
-  static Database? _database;
-
-  DatabaseHelper._init();
-
-  Future<Database> get database async {
-    if (_database != null) return _database!;
-    _database = await _initDB('signup.db');
-    return _database!;
-  }
-
-  Future<Database> _initDB(String filePath) async {
-    final dbPath = await getDatabasesPath();
-    final path = join(dbPath, filePath);
-    return await openDatabase(path, version: 1, onCreate: _createDB);
-  }
-
-  Future<void> _createDB(Database db, int version) async {
-    await db.execute('''
-      CREATE TABLE user(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        email TEXT,
-        password TEXT
-      )
-    ''');
-  }
-
-  Future<int> insertUser(String email, String password) async {
-    final db = await instance.database;
-    return await db.insert('user', {'email': email, 'password': password});
-  }
-
-  Future<Map<String, dynamic>?> getUser() async {
-    final db = await instance.database;
-    final result = await db.query('user', limit: 1, orderBy: 'id DESC');
-    return result.isNotEmpty ? result.first : null;
-  }
-
-  Future close() async {
-    final db = await instance.database;
-    db.close();
-  }
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Signup with Sqflite',
-      debugShowCheckedModeBanner: false,
-      home: SignUpScreen(),
+  static Future<Database> initDB() async {
+    if (_db != null) return _db!;
+    String path = join(await getDatabasesPath(), 'subject_data.db');
+    _db = await openDatabase(
+      path,
+      version: 1,
+      onCreate: (db, version) async {
+        await db.execute('''
+          CREATE TABLE subjects (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            subjectName TEXT,
+            course TEXT,
+            marks INTEGER,
+            creditHour REAL,
+            semester INTEGER
+          )
+        ''');
+      },
     );
-  }
-}
-
-class SignUpScreen extends StatefulWidget {
-  @override
-  _SignUpScreenState createState() => _SignUpScreenState();
-}
-
-class _SignUpScreenState extends State<SignUpScreen> {
-  final _emailController = TextEditingController();
-  final _passwordController = TextEditingController();
-
-  void _signUp() async {
-    String email = _emailController.text.trim();
-    String password = _passwordController.text.trim();
-
-    if (email.isNotEmpty && password.isNotEmpty) {
-      await DatabaseHelper.instance.insertUser(email, password);
-      Navigator.push(
-        context as BuildContext,
-        MaterialPageRoute(builder: (context) => HomeScreen()),
-      );
-    } else {
-      ScaffoldMessenger.of(context as BuildContext).showSnackBar(
-        SnackBar(content: Text('Please enter Email and Password')),
-      );
-    }
+    return _db!;
   }
 
-  @override
-  void dispose() {
-    _emailController.dispose();
-    _passwordController.dispose();
-    super.dispose();
+  static Future<int> insertData(Map<String, dynamic> data) async {
+    final db = await initDB();
+    return await db.insert('subjects', data);
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Sign Up')),
-      body: Padding(
-        padding: EdgeInsets.all(20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(labelText: 'Email'),
-            ),
-            SizedBox(height: 10),
-            TextField(
-              controller: _passwordController,
-              decoration: InputDecoration(labelText: 'Password'),
-              obscureText: true,
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _signUp,
-              child: Text('Sign Up'),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  Future<Map<String, dynamic>?> fetchUser() async {
-    return await DatabaseHelper.instance.getUser();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Home Screen')),
-      body: FutureBuilder<Map<String, dynamic>?>(
-        future: fetchUser(),
-        builder: (BuildContext context, AsyncSnapshot<Map<String, dynamic>?> snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasData && snapshot.data != null) {
-            return Center(
-              child: Text(
-                'Email: ${snapshot.data!['email']}\nPassword: ${snapshot.data!['password']}',
-                style: TextStyle(fontSize: 18),
-                textAlign: TextAlign.center,
-              ),
-            );
-          } else {
-            return Center(child: Text('No user found.'));
-          }
-        },
-      ),
-    );
+  static Future<List<Map<String, dynamic>>> getData() async {
+    final db = await initDB();
+    return await db.query('subjects');
   }
 }
