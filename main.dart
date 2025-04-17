1c1ea8dbbd39475cc75459b0ef882eeaefb1c6dd
ashh735 <riasatayesha4@gmail.com>
2025-04-17 04:54:24 -0700

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,25 +1,39 @@
 import 'package:flutter/material.dart';
-import 'home_screen.dart';
-import 'bgnu_map_screen.dart';
-import 'search_map_screen.dart';
+import 'qr_scanner_screen.dart';
+import 'qr_generator_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomeScreen(),
+    routes: {
+      '/scan': (context) => QRScannerScreen(),
+      '/generate': (context) => QRGeneratorScreen(),
+    },
+  ));
 }
 
-class MyApp extends StatelessWidget {
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'University Maps',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return Scaffold(
+      appBar: AppBar(title: Text('QR App')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              child: Text('Scan QR Code'),
+              onPressed: () => Navigator.pushNamed(context, '/scan'),
+
+            ),
+            ElevatedButton(
+              child: Text('Generate QR Code'),
+              onPressed: () => Navigator.pushNamed(context, '/generate'),
+            ),
+          ],
+        ),
       ),
-      home: HomeScreen(),
-      routes: {
-        '/bgnu': (context) => BGNUMapScreen(),
-        '/search': (context) => SearchMapScreen(),
-      },
     );
   }
 }
