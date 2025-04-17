38a4e3f34f9efa2ea7b1e0ea3c36f84fd6a08f7f
ashh735 <riasatayesha4@gmail.com>
2025-04-17 05:18:59 -0700

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,39 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'qr_scanner_screen.dart';
-import 'qr_generator_screen.dart';
+import 'home_screen.dart';
 
-void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: HomeScreen(),
-    routes: {
-      '/scan': (context) => QRScannerScreen(),
-      '/generate': (context) => QRGeneratorScreen(),
-    },
-  ));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
 }
 
-class HomeScreen extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('QR App')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              child: Text('Scan QR Code'),
-              onPressed: () => Navigator.pushNamed(context, '/scan'),
-
-            ),
-            ElevatedButton(
-              child: Text('Generate QR Code'),
-              onPressed: () => Navigator.pushNamed(context, '/generate'),
-            ),
-          ],
-        ),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Firebase Demo',
+      theme: ThemeData(primarySwatch: Colors.teal),
+      home: const HomeScreen(),
     );
   }
 }
