3539bdf619631bccaf1237ca5ec836567e1a6039
ashh735 <riasatayesha4@gmail.com>
2025-05-15 09:15:19 -0700

    Add files via upload

--- db_helper.dart
+++ db_helper.dart
@@ -1,38 +1,47 @@
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
 
 class DBHelper {
-  static Database? _db;
+  static final DBHelper _instance = DBHelper._internal();
+  factory DBHelper() => _instance;
 
-  static Future<Database> initDB() async {
+  DBHelper._internal();
+
+  Database? _db;
+
+  Future<Database> get database async {
     if (_db != null) return _db!;
-    String path = join(await getDatabasesPath(), 'subject_data.db');
-    _db = await openDatabase(
+    _db = await _initDB();
+    return _db!;
+  }
+
+  Future<Database> _initDB() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentsDirectory.path, "mydb.db");
+
+    return await openDatabase(
       path,
       version: 1,
       onCreate: (db, version) async {
         await db.execute('''
-          CREATE TABLE subjects (
+          CREATE TABLE texts (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            subjectName TEXT,
-            course TEXT,
-            marks INTEGER,
-            creditHour REAL,
-            semester INTEGER
+            content TEXT
           )
         ''');
       },
     );
-    return _db!;
   }
 
-  static Future<int> insertData(Map<String, dynamic> data) async {
-    final db = await initDB();
-    return await db.insert('subjects', data);
+  Future<void> insertText(String text) async {
+    final db = await database;
+    await db.insert('texts', {'content': text});
   }
 
-  static Future<List<Map<String, dynamic>>> getData() async {
-    final db = await initDB();
-    return await db.query('subjects');
+  Future<List<Map<String, dynamic>>> getTexts() async {
+    final db = await database;
+    return await db.query('texts');
   }
 }
