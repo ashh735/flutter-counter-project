a5db1829dd012561eb59096499746ee40e2d99af
ashh735 <riasatayesha4@gmail.com>
2025-05-15 09:07:25 -0700

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,20 +1,55 @@
 import 'package:flutter/material.dart';
-import 'home_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(PakistanFlagApp());
 }
 
-class MyApp extends StatelessWidget {
+class PakistanFlagApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Subject App',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
+      title: 'Pakistan Flag App',
+      theme: ThemeData(primarySwatch: Colors.green),
       home: HomeScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Home')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => FlagScreen()),
+            );
+          },
+          child: Text('Show Pakistan Flag'),
+        ),
+      ),
+    );
+  }
+}
+
+class FlagScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Pakistan Flag')),
+      body: Center(
+        child: Image.network(
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Flag_of_Pakistan.svg/320px-Flag_of_Pakistan.svg.png',
+          width: 300,
+          errorBuilder: (context, error, stackTrace) {
+            return Text('Failed to load image');
+          },
+        ),
+      ),
     );
   }
 }
